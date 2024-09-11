798e8965e9d42bc6576e08699f160c072e9a6d2e
Jefferson <jefferson.silvaquinto@gmail.com>
2024-09-11 14:27:09 -0500

    Correcting ubication login test

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,9 +1,9 @@
 import { expect, test, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthContext } from '../context/authContext.js';
-import { UserContext } from '../context/userContext.js';
-import Login from '../pages/Login/Login.js';
+import { AuthContext } from '../../context/authContext.js';
+import { UserContext } from '../../context/userContext.js';
+import Login from './Login.js';
 
 test('renders login form with document type (DNI/RUC) and phone number', () => {
   render(
@@ -28,8 +28,8 @@ test('renders login form with document type (DNI/RUC) and phone number', () => {
 });
 
 test('handles form submission with DNI and phone number', () => {
-  const mockLogin = vi.fn(); // Simular la función login
-  const mockUpdateUser = vi.fn(); // Simular la función updateUser
+  const mockLogin = vi.fn();
+  const mockUpdateUser = vi.fn();
 
   render(
     <Router>
@@ -48,22 +48,18 @@ test('handles form submission with DNI and phone number', () => {
   const checkbox2 = screen.getByTestId('input-comunication-policy');
 
   // Simular interacción con el formulario
-  fireEvent.change(documentInput, { target: { value: '12345678' } }); // DNI válido de 8 dígitos
-  fireEvent.change(phoneInput, { target: { value: '987654321' } }); // Número celular válido de 9 dígitos
+  fireEvent.change(documentInput, { target: { value: '12345678' } });
+  fireEvent.change(phoneInput, { target: { value: '987654321' } });
   fireEvent.click(checkbox1);
   fireEvent.click(checkbox2);
 
-  fireEvent.click(submitButton);
+  fireEvent.click(submitButton);  
 
-  console.log('documentInput: ', documentInput);
-  console.log('value: ', mockUpdateUser.mock.calls);
-
-  // Verificar que updateUser y login fueron llamados con los parámetros correctos
   expect(mockUpdateUser).toHaveBeenCalledWith({
     document: '12345678',
     number: '987654321',
     documentType: 'dni',
   });
 
-  expect(mockLogin).toHaveBeenCalled(); // Verifica que se llamó a la función login
+  expect(mockLogin).toHaveBeenCalled();
 });
